da5345cd70ba2b636893a79567269a229eb82566
Ilya <karpov.ilja@gmail.com>
2025-04-25 00:19:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Lidl Iepirkumu Grozs
+
+Šī Python programma ir iepirkumu grozs, kas apstrādā produktu datus no CSV faila un piedāvā dažādas funkcijas, piemēram, produktu apskati, meklēšanu un aprēķinus.
+
+## Projekta struktūra
+
+- **`lidl.csv`**: CSV fails, kas satur produktu datus, piemēram, nosaukumu, zīmolu, cenu un atlaidi.
+- **`main.py`**: Python skripts, kas nodrošina lietotāja saskarni un funkcionalitāti darbam ar produktu datiem.
+
+## Vertēšanas kritēriji
+Katrs punkts no saraksta zemāk ir 1 balle. Ja prasība realizēta daļeji - var būt 0.5 balles.
+
+### Nefunkcionālas prasības
+
+1. Kodam ir jēdzīgi komentāri, pirms "if, for, while" koda konstrukcijam
+2. Koda palaišanas brīdī nerādās kļūdas
+3. Mainīgo un funkciju nosaukumi atspoguļo izmantošanas būtību, bez saisinājumiem, rakstīti snake_case stilā
+
+### Funkcionālas prasības
+
+Programma piedāvā šādas iespējas:
+1. Parādīt visus produktus.
+2. Parādīt konkrētu produktu pēc tā kārtas numura.
+3. Pievienot jaunu produktu sarakstam.
+4. Parādīt top 3 lētākos produktus.
+5. Parādīt top 3 produktus ar lielāko atlaidi.
+6. Meklēt produktus pēc zīmola.
+7. Aprēķināt un parādīt gala summu ar atlaidēm.
---
+++ main.py
@@ -0,0 +1,74 @@
+import csv
+
+# Inicializē tukšu sarakstu, lai saglabātu produktu datus
+grozs = []
+
+# Atver CSV failu, kurā ir produktu dati
+with open('lidl.csv', newline='', encoding='utf-8') as csv_file:
+    file_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
+
+    # Nolasīt katru rindu no CSV faila un pievienot to sarakstam
+    for row in file_reader:
+        grozs.append(row)
+
+grozs.pop(0)
+
+while True:
+    # Aicināt lietotāju ievadīt komandu
+    choice = input("Ievadi komandu: ")
+
+    # Parādīt izvēlnes opcijas
+    print("1. Visi produkti")
+    print("2. Produkts pēc indeksa")
+    print("3. Pievienot jaunu produktu")
+    print("4. Top 3 letākie produkti")
+    print("5. Top 3 produkti ar lielāku atlaidi")
+    print("6. Meklēt pēc zīmola")
+    print("7. Gala summa ar atlaidi")
+
+    # Opcija 1: Parādīt visus produktus
+    if choice == "1":
+        # ar ciklu iziet cauri produktiem un izdrukā katru no tiem
+        # katram produktam izdrukā nosaukumu, atlaidi un cenu ar atlaidi
+        pass
+
+    # Opcija 2: Parādīt produktu pēc kārtas numura
+    elif choice == "2":
+        # pajautā lietotājam ievadīt kārtas numuru
+        # paņem produktu no saraksta ar norādīto kārtas numuru izmantot saraksta indeksu
+        pass
+
+    # Opcija 3: Pievienot jaunu produktu sarakstam
+    elif choice == "3":
+        # pajauta lietotājam ievadīt produkta datus
+        # izveido jaunu produktu - sarakstu ar ievadītajiem datiem
+        # pievieno to grozam
+        pass
+    
+    # Opcija 4: Parādīt top 3 lētākos produktus
+    elif choice == "4":
+        # izmanto kārtošanu un sarakta griešanu, lai iegūtu 3 lētākos produktus
+        pass
+    
+    # Opcija 5: Parādīt top 3 produktus ar lielāko atlaidi
+    elif choice == "5":
+        # izmanto kārtošanu un sarakta griešanu, lai produktus ar lielāko atlaidi
+        pass
+
+    # Opcija 6: Meklēt produktus pēc zīmola
+    elif choice == "6":
+        # pajautā lietotājam ievadīt zīmolu
+        # ar ciklu iziet cauri produktiem un pārbauda, vai zīmols atbilst meklējamajam izmantojot "in" operatoru
+        pass
+    
+    # Opcija 7: Aprēķināt un parādīt katram produktam gala summu ar atlaidēm un kopējo atlaižu summu izmantojot cenu, cenu ar atlaidi un atlaidi procentos
+    elif choice == "7":
+        # ar ciklu un summēšanu aprēķina katram produktam noradītas summas
+        pass
+
+    # Apstrādāt nederīgas komandas
+    else:
+        break
+    
+    # Izdrukāt atdalītāju labākai lasāmībai
+    print("==========================")
